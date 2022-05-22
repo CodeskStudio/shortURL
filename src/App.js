ddf1694519c6152b25e486ff708265d2453731c9
CodeskStudio <code@codesk.studio>
2022-05-22 17:10:26 +0200

    basic redict

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+
+
+var currentURL = window.location.pathname;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App center header">{currentURL}
     </div>
   );
 }
+
+// Essential Chrome Extension
+
+if (currentURL === "/ig") {
+   window.location.href = 'https://www.instagram.com/codesk.studio'; 
+}
+
+if (currentURL === "/chex") {
+  window.location.href = 'https://www.instagram.com/p/Cd23LdJLwRl/'; 
+}
+
 
 export default App;
